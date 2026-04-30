f6846c3f2964a6ef9d8c43705f6e0e64a10e6686
Andrechotech <69160191+andresdiazf@users.noreply.github.com>
2026-04-29 21:56:53 -0500

    Pruebas para clase producto

--- src/main/java/org/example/Producto.java
+++ src/main/java/org/example/Producto.java
@@ -4,15 +4,15 @@ public class Producto {
     private String nombre;
     private double precio;
     private int stock;
-    
+
     public Producto(String nombre, double precio, int stock) {
-        if (precio < 0) throw new IllegalArgumentException("El precio no puede ser negativo"
-        );
+        if (precio < 0) throw new IllegalArgumentException("El precio no puede ser negativo");
         if (stock < 0) throw new IllegalArgumentException("El stock no puede ser negativo");
         this.nombre = nombre;
         this.precio = precio;
         this.stock = stock;
     }
+
     public String getNombre() { return nombre; }
     public double getPrecio() { return precio; }
     public int getStock() { return stock; }
--- src/test/java/ProductoTest.java
+++ src/test/java/ProductoTest.java
@@ -1,19 +1,51 @@
 import org.example.Producto;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductoTest {
 
     @Test
-    void testPruebaProducto() {
-// Organizar
+    void precioProducto()
+    {
+        // Organizar
         Producto producto = new Producto("Cuaderno", 5000, 10);
-// Actuar
+        // Actuar
         double precio = producto.getPrecio();
-// Afirmar
+        // Afirmar
         assertEquals(5000, precio);
     }
 
-    private void assertEquals(int i, double precio) {
+    @Test
+    void productoStockMayorACero()
+    {
+        Producto producto = new Producto("Lapiz", 2000, 5);
+        boolean disponible = producto.estaDisponible();
+        assertTrue(producto.estaDisponible());
     }
+
+    @Test
+    void productoStockNoDisponible()
+    {
+        Producto producto = new Producto("Borrador", 1000, 0);
+
+        assertFalse(producto.estaDisponible());
+    }
+
+    @Test
+    void precioNegativoLanzaError() {
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Producto("Teclado", -1000, 5);
+        });
+    }
+
+    @Test
+    void productoNegativoLanzaError() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Producto("Pantalla", 50000, -3);
+        });
+    }
+
 
 }
 
